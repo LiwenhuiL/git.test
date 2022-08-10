067ed726121348230630d2b99cb5ce7ec37f7591
Li <Li@atguigu.com>
2022-08-10 17:21:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @Description
+ * @create 2022-08-10 17:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
